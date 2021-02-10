dc9ca37777114a8213c2e564cb4bc3d89fe2e913
tema-969 <69858987+tema-969@users.noreply.github.com>
2021-02-10 23:05:57 +0300

    add checkWin

---
+++ src/main/java/ru/geekbrains/lesson7/XoForm.java
@@ -0,0 +1,49 @@
+package ru.geekbrains.lesson7;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class XoForm extends JFrame {
+    private GameField gameField;
+
+    public XoForm() {
+        this.setTitle("X-O");
+        this.setBounds(500, 300, 381, 460);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.gameField = new GameField();
+        this.add(gameField);
+
+        JPanel bottomPanel = new JPanel(new GridLayout(1,2));
+        Button btnStart = new Button("Start New Game");
+        Button btnExit = new Button("Exit Game");
+        bottomPanel.add(btnStart);
+        bottomPanel.add(btnExit);
+        bottomPanel.setPreferredSize(new Dimension(1,40));
+        this.add(bottomPanel, BorderLayout.SOUTH);
+
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                gameField.startGame();
+            }
+        });
+
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        this.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new XoForm();
+
+    }
+}
